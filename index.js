5ce120592c51f5e918514e14837732bd045d45f5
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-11-25 08:58:08 +0000

    index

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+// Routes
+app.get('/', (req, res) => {
+    res.send('Welcome to PoyoTube!');
+});
+
+app.get('/videos', (req, res) => {
+    // Logic to fetch and return a list of videos
+    res.send('List of videos');
+});
+
+app.post('/upload', (req, res) => {
+    // Logic to handle video upload
+    res.send('Video uploaded');
+});
+
+app.get('/videos/:id', (req, res) => {
+    const videoId = req.params.id;
+    // Logic to fetch and return a specific video by ID
+    res.send(`Video with ID: ${videoId}`);
+});
+
+app.listen(port, () => {
+    console.log(`PoyoTube app listening at http://localhost:${port}`);
+});
